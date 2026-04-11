f1da88b4c06e93ac1fe4d914d4df40f67bbc17ce
fedor <feedoor.feedoot@gmail.com>
2026-04-12 01:18:28 +0300

    feature: start front

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
 import FooterMenu from './components/FooterMenu.vue'
+import HeaderMenu from './components/HeaderMenu.vue';
+import Advert from './components/Advert.vue';
 </script>
 
 <template>
+  <HeaderMenu />
+  <Advert />
   <FooterMenu />
 </template>
---
+++ frontend/src/components/HeaderMenu.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Home, SlidersHorizontal, ShoppingCart } from 'lucide-vue-next'
+</script>
+
+<template>
+  <div class="header-menu">
+    <div class="search-box">
+      <Home :size="24" />
+      <input type="text" placeholder="Поиск" class="search-input" />
+      <SlidersHorizontal :size="24" />
+    </div>
+    <ShoppingCart :size="24" />
+  </div>
+</template>
