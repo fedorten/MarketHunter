48fed7f1b4f02358caa1959e1ef51cb68e39c457
fedor <feedoor.feedoot@gmail.com>
2026-04-13 15:16:17 +0300

    front start

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import FooterMenu from './components/FooterMenu.vue'
-import HeaderMenu from './components/HeaderMenu.vue';
-import Advert from './components/Advert.vue';
+import HeaderMenu from './components/HeaderMenu.vue'
+import Advert from './components/Advert.vue'
 </script>
 
 <template>
